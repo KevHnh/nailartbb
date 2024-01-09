eae17ff5857d478afae7997bc907cb89461ba105
KevHnh <kevinhinh9@gmail.com>
2024-01-09 01:13:16 -0500

    updates

---
+++ src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="LandingPageContainer">
+      <div className="LandingPageWrapper">
+        <div className="title">BB Nails</div>
+        <div className="btn">Book Appointment</div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
