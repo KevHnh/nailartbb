a2edc4ca387eba79ac4aa03bc3bf48bff7c8d6a3
KevHnh <kevinhinh9@gmail.com>
2024-01-13 23:23:10 -0500

    updates

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./FAQ.css";
 
 function FAQ() {
-  return <div></div>;
+  return (
+    <div className="GlobalContainer">
+      <div className="GlobalWrapper">
+        <div className="header">FAQ and Policies</div>
+        <div className="sub1">
+          Thank you for choosing me as your nail technician here are my policies
+          that will help your appointment (and my day) run smoothly
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FAQ;
